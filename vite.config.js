a3ff782c0c7970060658020b0295eeafeb02582a
lperaza2 <lperaza2@stu.southtexascollege.edu>
2026-02-11 22:11:07 -0600

    wrangler config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
+import { cloudflare } from '@cloudflare/vite-plugin'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
